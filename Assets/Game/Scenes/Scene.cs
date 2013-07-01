7787fb82b58a6921022fe30383caf3d00e5917f8
Thomas Muntaner <thomas.muntaner@gmail.com>
2013-07-01 16:29:29 +0200

    initial commit

---
+++ Assets/Game/Scenes/Scene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Scene : FContainer
+{
+	virtual public void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	virtual public void Update ()
+	{
+	
+	}
+}
+
